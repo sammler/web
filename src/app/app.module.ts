ff49a0b861c980b1852f6a8b29b60b2882462879
Stefan Walther <swr@qlik.com>
2018-10-05 16:50:07 +0200

    feat: first skeleton using Clarity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {LogListComponent} from "./logs/log-list.component";
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ClarityModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/logs/log-list.component.ts
@@ -0,0 +1,11 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'log-list',
+  templateUrl: './log-list.component.html',
+  styleUrls: ['./log-list.component.css']
+})
+export class LogListComponent {
+  title = 'web';
+}
