f7cb9f54de7bb5cfc73a3b904074527de857a057
Stefan Walther <swr@qlik.com>
2018-10-15 23:48:06 +0200

    refactor: move logs to admin module

---
+++ src/app/admin/admin.routes.ts
@@ -0,0 +1,7 @@
+import {Routes} from '@angular/router';
+
+import {LogsComponent} from './logs/logs.component';
+
+export const adminRoutes: Routes = [
+  {path: 'admin/logs', component: LogsComponent  },
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,38 +4,35 @@ import {RouterModule} from '@angular/router';
 import {ClarityModule} from '@clr/angular';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+import {CoreModule} from './core/core.module';
+import {UserModule} from './user/user.module';
+import {AdminModule} from './admin/admin.module';
+
 import {AppComponent} from './app.component';
-
-import {appRoutes} from './app.routes';
-
 import {SideNavComponent} from './shared/side-nav/side-nav.component';
 import {HeaderComponent} from './shared/header/header.component';
 
+import {appRoutes} from './app.routes';
+
+
 import {HomeComponent} from './home/home.component';
-
-import {LogsComponent} from './admin/logs/logs.component';
-import {AdminComponent} from './admin/admin.component';
-
 import {HeartbeatComponent} from './user/strategy/heartbeat/heartbeat.component';
 import {TwitterComponent} from './user/strategy/twitter/twitter.component';
 import {RssComponent} from './user/strategy/rss/rss.component';
 import {GithubComponent} from './user/strategy/github/github.component';
 import {StrategyGithubDashboard} from './user/strategy/github/dashboard/strategy-github-dashboard.component';
 import {StrategyGithubSettings} from './user/strategy/github/settings/strategy-github-settings.component';
-import {CoreModule} from './core/core.module';
-import {UserModule} from './user/user.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogsComponent,
     SideNavComponent,
     HeaderComponent,
     HeartbeatComponent,
     TwitterComponent,
     RssComponent,
     GithubComponent,
-    AdminComponent,
     HomeComponent,
     StrategyGithubDashboard,
     StrategyGithubSettings
@@ -46,7 +43,8 @@ import {UserModule} from './user/user.module';
     ClarityModule,
     BrowserAnimationsModule,
     CoreModule,
-    UserModule
+    UserModule,
+    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
