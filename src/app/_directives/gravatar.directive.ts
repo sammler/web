46493d7c96b9e2ac32a6938a0426ccd2556cdbef
Stefan Walther <swr@qlik.com>
2018-10-24 12:09:21 +0200

    feat: gravatar directive

---
+++ src/app/_directives/gravatar.directive.ts
@@ -0,0 +1,28 @@
+import {Directive, ElementRef, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import * as md5 from 'md5';
+
+@Directive({
+  selector: '[gravatar]'
+})
+export class GravatarDirective implements OnInit, OnChanges {
+
+  @Input('email') email: string;
+  @Input('size') size: number = 36;
+  @Input('fallbaack') fallback: string = 'mm';
+
+  constructor(private elementRef: ElementRef) {
+
+  }
+
+  ngOnInit() {
+    this.setUrlGravatar(this.email);
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+
+  }
+
+  setUrlGravatar(email: string) {
+    this.elementRef.nativeElement.src = `//www.gravatar.com/avatar/${md5(email)}?s=${this.size}`;
+  }
+}
